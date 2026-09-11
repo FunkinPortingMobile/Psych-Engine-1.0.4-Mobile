91fc9e3d4f2aca9fcd96b498081e6814c7472270
StarNovaBR <cherrylegal001@gmail.com>
2026-09-11 17:38:00 -0300

    okay

--- source/states/ModsMenuState.hx
+++ source/states/ModsMenuState.hx
@@ -787,7 +787,7 @@ class ModsMenuState extends MusicBeatState
 		}
 
 		var path:String = 'modsList.txt';
-		File.saveContent(path, fileStr);
+		File.saveContent(#if mobile StorageSystem.getDirectory() + #end path, fileStr);
 		Mods.parseList();
 		Mods.loadTopMod();
 	}
